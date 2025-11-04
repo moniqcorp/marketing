65edb1bfc3316487a84868abc8780f2784d9cecf
lsh0107 <zippoo94@naver.com>
2025-11-04 22:06:14 +0900

    add(toss): 토스 에러 모듈 기본 정의 추가

---
+++ app/common/errors.py
@@ -0,0 +1,14 @@
+class TossError(Exception):
+    """Toss 스크래핑 중 발생하는 주요 에러."""
+
+    def __init__(self, message: str, code: int = 500):
+        super().__init__(message)
+        self.code = code
+        self.message = message
+
+    def __str__(self):
+        return f"[Code {self.code}] {self.message}"
+
+    def to_dict(self):
+        """에러를 dict 형태로 반환"""
+        return {"code": self.code, "message": self.message}
